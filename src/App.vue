8c6910801327dedbaa87023c34d36bf087ac3f70
Pedro Andrade <86939238+ppedroandrade@users.noreply.github.com>
2023-06-15 01:47:25 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="top">
+        <nav>
+          <div class="btn-group-vertical">
+            <RouterLink to="/">
+              <button class="btn btn-primary">Home</button>
+            </RouterLink>
+            <RouterLink to="/about">
+              <button class="btn btn-primary">About</button>
+            </RouterLink>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.top {
+  margin: 10px;
+  padding: 10px;
+}
+
+.btn-group-vertical {
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h1 class="text-center mt-5">Sistema de Estacionamento</h1>
+    <div class="row mt-5">
+      <div class="col-md-6 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Registro de Veículos</h5>
+            <p class="card-text">Registre a entrada de veículos no estacionamento.</p>
+            <router-link to="/registro" class="btn btn-primary">Registrar Veículo</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Lista de Veículos</h5>
+            <p class="card-text">Visualize a lista de veículos registrados.</p>
+            <router-link to="/lista" class="btn btn-primary">Ver Lista</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Lista de Condutores</h5>
+            <p class="card-text">Visualize a lista de Condutores registrados.</p>
+            <router-link to="/lista" class="btn btn-primary">Ver Lista</router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Registro de Condutores</h5>
+            <p class="card-text">Registre a entrada de Condutores no estacionamento.</p>
+            <router-link to="/lista" class="btn btn-primary">Ver Lista</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.container {
+  max-width: 600px;
+}
+
+.mb-4 {
+  margin-bottom: 1.5rem;
+}
+</style>
